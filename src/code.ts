9df9057b00fd03b99659b521cc6dd7038b43e1b7
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-09-20 22:04:29 +0100

    feat: demonstrate Subjects

--- src/code.ts
+++ src/code.ts
@@ -1,11 +1,22 @@
-import { Observable } from "rxjs-compat/Observable";
-import { fromEvent } from "rxjs-compat/Observable/fromEvent";
+import { Subject } from "rxjs-compat/Subject";
 
-const observable = fromEvent(document, "mousemove");
+const subject = new Subject();
 
-setTimeout(() => {
-  const subscription = observable.subscribe((x: any) => addItem(x));
-}, 2000);
+subject.subscribe(
+  (data) => addItem(`Observer 1: ${data}`),
+  (err) => addItem(err),
+  () => addItem("Observer 1 completed!")
+);
+
+subject.next("The first thing has been sent");
+
+const observer2 = subject.subscribe((data) => addItem(`Observer 2: ${data}`));
+
+subject.next("The second thing has been sent");
+subject.next("A third thing has been sent");
+
+observer2.unsubscribe(); // doesn't get subsequent data anymore
+subject.next("A final thing has been sent");
 
 function addItem(val: any) {
   const node = document.createElement("li");
