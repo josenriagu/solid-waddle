6278b757d27ebdcd6a2f07470b222e0a8a1a5249
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-09-20 19:05:29 +0100

    feat: cancel an observer using unsubscribe method

--- src/code.ts
+++ src/code.ts
@@ -5,19 +5,25 @@ const observable = Observable.create((observer: any) => {
     // observers read values coming from the observables
     observer.next("Hey guys!");
     observer.next("How are you?");
-    observer.complete();
-    observer.next("This will not show!");
+    setInterval(() => {
+      observer.next("We are good 🙌");
+    }, 2000);
   } catch (err) {
     observer.error(err);
   }
 });
 
 // subscribe takes 3 arguments, one for next, one for error and one for complete
-observable.subscribe(
+const observer = observable.subscribe(
   (x: any) => addItem(x),
   (error: any) => addItem(error),
   () => addItem("Completed!")
 );
+
+// to cancel an observer, assign it to a variable and call the unsubscribe method
+setTimeout(() => {
+  observer.unsubscribe();
+}, 6001);
 
 function addItem(val: any) {
   const node = document.createElement("li");
