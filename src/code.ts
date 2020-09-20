675e04004335249152f2dc185c9561e773a0c108
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-09-20 17:35:59 +0100

    refactor: observe error in catch block

--- src/code.ts
+++ src/code.ts
@@ -1,11 +1,15 @@
 import { Observable } from "rxjs-compat/observable";
 
 const observable = Observable.create((observer: any) => {
-  // observers read values coming from the observables
-  observer.next("Hey guys!");
-  observer.next("How are you?");
-  observer.complete();
-  observer.next("This will not show!");
+  try {
+    // observers read values coming from the observables
+    observer.next("Hey guys!");
+    observer.next("How are you?");
+    observer.complete();
+    observer.next("This will not show!");
+  } catch (err) {
+    observer.error(err);
+  }
 });
 
 // subscribe takes 3 arguments, one for next, one for error and one for complete
